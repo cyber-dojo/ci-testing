43fc322ed8915fd659c0c24acd519115421ef607
JonJagger <jon@kosli.com>
2026-03-13 11:40:20 +0000

    WIP for workflow to find new snyk vulns

--- bin/filter_sarif.py
+++ bin/filter_sarif.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python3
 
-import datetime
 import sys
 import json
-import yaml
+
 
 if __name__ == "__main__":  # pragma: no cover
     sarif_filename = sys.argv[1]
-    snyk_policy_filename = sys.argv[2]
 
     # Extract ids and severities of each vulnerability in sarif file
     with open(sarif_filename) as sarif_file:
         sarif_data = json.load(sarif_file)
 
-    epoch_start = datetime.datetime(1970, 1, 1, 0, 0, 0, 0, tzinfo=datetime.timezone.utc)
-
-    vulns = {}
+    vulns = []
     for run in sarif_data['runs']:
         for rule in run['tool']['driver']['rules']:
             id = rule['id']
@@ -23,34 +19,16 @@ if __name__ == "__main__":  # pragma: no cover
             short_text = rule['shortDescription']['text']
             #cvssv3_base_score = rule['properties']['cvssv3_baseScore'] # eg 6.8 can be None
             #security_severity = rule['properties']['security-severity'] # eg 6.8 can be None
-            severity = short_text.split(' ')[0]  # eg "Medium"
-            assert severity in ["Critical", "High", "Medium", "Low"]
+            severity = short_text.split(' ')[0].lower()  # eg "medium"
+            assert severity in ["critical", "high", "medium", "low"]
 
-            vulns[id] = {
+            vulns.append({
+                'id': id,
                 'severity': severity,
-                'url': url,
-                'expires': epoch_start
-            }
+                'url': url
+            })
 
-    # Overwrite specific vulnerability expiry dates if found in snyk policy file (yaml)
-    with open(snyk_policy_filename) as snyk_file:
-        snyk_data = yaml.safe_load(snyk_file)
-
-    ignore = snyk_data['ignore']
-    for id in ignore:
-        if id in vulns:
-            vulns[id]['expires'] = ignore[id][0]['*']['expires']
-
-    flat = []
-    for id, values in vulns.items():
-        flat.append({
-            'snyk_id': id,
-            'snyk_severity': values['severity'],
-            'snyk_url': values['url'],
-            'snyk_expires': values['expires']
-        })
-
-    print(json.dumps(flat, default=str))
+    print(json.dumps(vulns, default=str))
 
 
 #   Severity. CVSS v3 Rating
